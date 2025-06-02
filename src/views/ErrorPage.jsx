37797deeb6ade4fc31383a33a620a07a4da1917e
Joel Garcia <joel77270@gmail.com>
2025-06-02 20:13:15 -0300

    Se agregaron estilos al ErroPage [Joel Garcia]

--- src/views/ErrorPage.jsx
+++ src/views/ErrorPage.jsx
@@ -1,9 +1,39 @@
-const ErrorPage = () =>{
-    return(
-        <div>
-            <h1>Error 404</h1>
-        </div>
-    )
-}
+import { Box, Heading, Text, Button, VStack } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { useColorModeValue } from "@/components/ui/color-mode";
+
+const ErrorPage = () => {
+  const bgColor = useColorModeValue("gray.100", "gray.800");
+  const textColor = useColorModeValue("gray.600", "gray.300");
+  const headingColor = useColorModeValue("red.600", "red.600");
+
+  return (
+    <Box
+      minH="100vh"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      bg={bgColor}
+      px={4}
+    >
+      <VStack spacing={6} textAlign="center">
+        <Heading as="h1" size="4xl" color={headingColor}>
+          🚫 Error 404
+        </Heading>
+        <Text fontSize="xl" color={textColor}>
+          La página que buscas no existe o fue removida.
+        </Text>
+        <Text fontSize="lg" color={textColor}>
+          🔍 Verifica la URL o regresa al inicio.
+        </Text>
+        <Link to="/">
+          <Button colorScheme="teal" leftIcon={<span>🏠</span>}>
+            Volver al inicio
+          </Button>
+        </Link>
+      </VStack>
+    </Box>
+  );
+};
 
 export default ErrorPage;
